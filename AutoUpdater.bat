038f1c80d47054f1a921996dfd3d91cc98fcb503
sankaaku <92543349+sankaaku@users.noreply.github.com>
2021-10-14 11:19:01 -0700

    Add files via upload

---
+++ AutoUpdater.bat
@@ -0,0 +1,28 @@
+for /f "delims=" %%d in (.\dloc.) do set dloc=%%d
+for /f "delims=" %%r in (.\dver.) do set dver=%%r
+for /f "delims=" %%s in (.\sloc.) do set sloc=%%sSlimeVR
+for /f "delims=" %%e in (.\sver.) do set sver=%%e
+for /f "tokens=9,9 delims=/,>" %%D in (
+	'curl https://github.com/SlimeVR/SlimeVR-OpenVR-Driver/releases/latest ^|find "v"'
+) do set dnet=%%D
+for /f "tokens=9,9 delims=/,>" %%S in (
+	'curl https://github.com/SlimeVR/SlimeVR-Server/releases/latest ^|find "v"'
+) do set snet=%%S
+set dnet=%dnet:"=%
+set snet=%snet:"=%
+if %dver%==%dnet% (goto :cs) else (goto :ud)
+:ud
+del /q /s "%dloc%slimevr"
+powershell -Command "Invoke-WebRequest https://github.com/SlimeVR/SlimeVR-OpenVR-Driver/releases/download/%dnet%/slimevr-openvr-driver-win64.zip -OutFile SVRD.zip"
+powershell.exe -NoP -NonI -Command "Expand-Archive '.\SVRD.zip' '%dloc%'"
+del /q /s ".\SVRD.zip"
+echo %dnet%>.\dver.
+:cs
+if %sver%==%snet% (goto :l) else (goto :us)
+:us
+powershell -Command "Invoke-WebRequest https://github.com/SlimeVR/SlimeVR-Server/releases/download/%snet%/slimevr.jar -OutFile slimevr.jar"
+echo %snet%>.\sver.
+:l
+start javaw -Xmx512M -jar slimevr.jar
+timeout 3
+start steam://rungameid/250820
---
+++ SlimeVR_Installer.bat
@@ -0,0 +1,111 @@
+@echo off
+for /f "tokens=9,9 delims=/,>" %%d in (
+	'curl https://github.com/SlimeVR/SlimeVR-OpenVR-Driver/releases/latest ^|find "v"'
+) do set dnet=%%d
+for /f "tokens=9,9 delims=/,>" %%s in (
+	'curl https://github.com/SlimeVR/SlimeVR-Server/releases/latest ^|find "v"'
+) do set snet=%%s
+set dnet=%dnet:"=%
+set snet=%snet:"=%
+:start
+set dloc=C:\Program Files (x86)\Steam\steamapps\common\SteamVR\drivers\
+set sloc=C:\
+cls
+echo ------------------
+echo SlimeVR Installer
+echo Made by kaaku
+echo ------------------
+echo INSTALL - 1
+echo UNINSTALL - 2
+echo ------------------
+set /p uniins=
+if %uniins%==1 goto :ins
+if %uniins%==2 goto :uni
+if %uniins%== goto :start
+:uni
+cls
+echo CHANGE DRIVER UNINSTALL DIRECTORY
+echo PRESS ANY KEY WITH NO TEXT TO SKIP
+echo MAKE SURE DIRECTORY ENDS WITH \
+echo MAKE SURE DIRECTORY DOES NOT REQUIRE ADMIN RIGHTS
+echo DEFAULT DIRECTORY: %dloc%
+set /p dloc=
+cls
+echo SERVER UNINSTALL DIRECTORY
+echo PRESS ANY KEY WITH NO TEXT TO SKIP
+echo MAKE SURE DIRECTORY ENDS WITH \
+echo MAKE SURE DIRECTORY DOES NOT REQUIRE ADMIN RIGHTS
+echo DEFAULT DIRECTORY: %sloc%
+set /p sloc=
+cls
+echo DRIVER: %dloc%
+echo SERVER: %sloc%
+echo ARE THESE CORRECT?
+echo [y/n] CASE SENSITIVE
+set /p dircon=
+if %dircon%==y goto :1
+if %dircon%==n goto :start
+if %dircon%== goto :start
+:1
+del /q /s "%userprofile%\Desktop\SlimeVR.lnk"
+del /q /s "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\SlimeVR.lnk"
+rmdir /q /s "%dloc%slimevr"
+rmdir /q /s "%sloc%SlimeVR"
+cls
+echo SlimeVR SUCCESSFULLY UNINSTALLED
+echo PRESS ANY KEY TO CLOSE && pause >nul
+exit
+:ins
+cls
+echo CHANGE DRIVER INSTALL DIRECTORY
+echo PRESS ANY KEY WITH NO TEXT TO SKIP
+echo MAKE SURE DIRECTORY ENDS WITH \
+echo MAKE SURE DIRECTORY DOES NOT REQUIRE ADMIN RIGHTS
+echo DEFAULT DIRECTORY: %dloc%
+set /p dloc=
+cls
+echo SERVER INSTALL DIRECTORY
+echo PRESS ANY KEY WITH NO TEXT TO SKIP
+echo MAKE SURE DIRECTORY ENDS WITH \
+echo MAKE SURE DIRECTORY DOES NOT REQUIRE ADMIN RIGHTS
+echo DEFAULT DIRECTORY: %sloc%
+set /p sloc=
+cls
+echo DRIVER: %dloc%
+echo SERVER: %sloc%
+echo ARE THESE CORRECT?
+echo [y/n] CASE SENSITIVE
+set /p dircon=
+if %dircon%==y goto :2
+if %dircon%==n goto :start
+if %dircon%== goto :start
+:2
+cls
+powershell -command "Invoke-WebRequest https://github.com/SlimeVR/SlimeVR-Server/releases/download/%snet%/SlimeVR.zip -OutFile svrs.zip"
+powershell -nop -noni -command "Expand-Archive '.\svrs.zip' '%sloc%'"
+del /q /s ".\svrs.zip"
+echo %snet%>"%sloc%SlimeVR\sver."
+powershell -command "Invoke-WebRequest https://github.com/SlimeVR/SlimeVR-OpenVR-Driver/releases/download/%dnet%/slimevr-openvr-driver-win64.zip -OutFile svrd.zip"
+powershell -nop -noni -command "Expand-Archive '.\svrd.zip' '%dloc%'"
+del /q /s ".\svrd.zip"
+echo %dnet%>"%sloc%SlimeVR\dver."
+echo %dloc%>"%sloc%SlimeVR\dloc."
+powershell -command "Invoke-WebRequest https://files.catbox.moe/eg5rxv.bat -OutFile run.bat"
+move ".\run.bat" "%sloc%SlimeVR"
+powershell -command "Invoke-WebRequest https://files.catbox.moe/vjwil9.ico -OutFile slime.ico"
+move ".\slime.ico" "%sloc%SlimeVR"
+echo Set oWS = WScript.CreateObject("WScript.Shell") > svrsc.vbs
+echo sLinkFile = "%userprofile%\Desktop\SlimeVR.lnk" >> svrsc.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> svrsc.vbs
+echo oLink.TargetPath = "%sloc%SlimeVR\run.bat" >> svrsc.vbs
+echo oLink.WorkingDirectory = "%sloc%SlimeVR" >> svrsc.vbs
+echo oLink.Description = "SlimeVR Launcher by kaaku" >> svrsc.vbs
+echo oLink.IconLocation = "%sloc%SlimeVR\slime.ico" >> svrsc.vbs
+echo oLink.Save >> svrsc.vbs
+cscript svrsc.vbs
+del /q /s ".\svrsc.vbs"
+xcopy "%userprofile%\Desktop\SlimeVR.lnk" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\"
+cls
+echo SlimeVR SUCCESSFULLY INSTALLED 
+echo PRESS ANY KEY TO CLOSE && pause >nul
+exit
